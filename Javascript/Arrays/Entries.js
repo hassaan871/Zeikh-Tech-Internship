426697c24886e99da7db5f0fd1b5de7fb0aa0bed
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-31 16:28:32 +0500

    don't get what exactly is arraus entries function but used it, didn't get the exact concept

---
+++ Javascript/Arrays/Entries.js
@@ -0,0 +1,32 @@
+// const fruits = ["apple","banana","mango","pineapple"];
+// const list = fruits.entries();
+// // console.log(list);
+// let text = "";
+// for(let x of list){
+//     text += x;
+// }
+// console.log(text);
+
+// const nums = [1,2,3,4];
+// const list = nums.entries();
+// let sum = 0;
+// for(let x of nums){
+//     sum += x;
+// }
+// console.log(sum);
+
+const nums = [1,2,3,4];
+const nums2 = new Array([5,6,7,8]);
+const list = nums.entries();
+let sum = 0;
+for(let x of list){
+    sum += x;
+}
+// console.log(sum);
+console.log(list);
+
+// console.log('Type of sum: ',typeof sum);
+// console.log('Type of Nums: ',typeof nums);
+// console.log('Type of Nums2: ',typeof nums);
+
+
