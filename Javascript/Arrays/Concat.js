1c1d8db79b0d095b0f0b2be3c0361229ccc7ae1a
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-31 14:59:27 +0500

    nested array concatinate

--- Javascript/Arrays/Concat.js
+++ Javascript/Arrays/Concat.js
@@ -7,8 +7,14 @@ const result = watches.concat(luxuryWatches);
 const superConcat = watches.concat(luxuryWatches, superLuxuryWatch);
 const strAndNums = nums.concat(watches);
 
+const arr1 = [1,2,[3,4]];
+const arr2 = [[5,6],7,8];
+const concat = arr1.concat(arr2);
+
 console.log(result);
 console.log(superConcat);
 console.log(strAndNums);
+console.log(concat);
+
 
 
