13d28bb1afb598b2ff7ee85778f26a4789cb69cf
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-31 14:13:08 +0500

    concatination

--- Javascript/Arrays/Concat.js
+++ Javascript/Arrays/Concat.js
@@ -1,5 +1,10 @@
 const watches = ["casio", "citizen"];
 const luxuryWatches = ["rolex", "rado"];
+const superLuxuryWatch = ["Patek Philippe"];
 
 const result = watches.concat(luxuryWatches);
+const superConcat = watches.concat(luxuryWatches, superLuxuryWatch);
+
 console.log(result);
+console.log(superConcat);
+
