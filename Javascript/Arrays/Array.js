d090b2f483efd2926fe8dab5f2eb391f4e2e2210
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-31 13:42:24 +0500

    just created some arrays

---
+++ Javascript/Arrays/Array.js
@@ -0,0 +1,11 @@
+// creates a new array 
+const bikes = ["cd 70", "cg 125", "ybr"];
+
+// create a new array and add values:
+const cars = [];
+cars.push("civic reborn");
+cars.push("civic rebirth");
+cars.push("civic x");
+
+// create an array with new Array() method 
+const sports = new Array(["basketball, volleyball, football"]);
