c77fed1a127110d1fab7466a358ab6fe810d2c61
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-31 14:15:33 +0500

    numbers and string arrays concatination

--- Javascript/Arrays/Concat.js
+++ Javascript/Arrays/Concat.js
@@ -1,10 +1,14 @@
 const watches = ["casio", "citizen"];
 const luxuryWatches = ["rolex", "rado"];
 const superLuxuryWatch = ["Patek Philippe"];
+const nums = [1,2,3];
 
 const result = watches.concat(luxuryWatches);
 const superConcat = watches.concat(luxuryWatches, superLuxuryWatch);
+const strAndNums = nums.concat(watches);
 
 console.log(result);
 console.log(superConcat);
+console.log(strAndNums);
+
 
