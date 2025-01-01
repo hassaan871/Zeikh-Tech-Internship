509445beadccba2dc8b2fed9dc29a488af442577
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 17:47:09 +0500

    Values method

---
+++ Javascript/Arrays/Day#02/Values.js
@@ -0,0 +1,15 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+const list = fruits.values();
+
+for(let x of list){
+    // console.log(x);
+    
+}
+
+for(let x of fruits.values()){
+    console.log(x);
+    
+}
+
+// console.log(list);
