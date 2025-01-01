127234f9b190decfc0d7d9911f4a03abfa07fef7
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 17:24:52 +0500

    ToString method

---
+++ Javascript/Arrays/Day#02/ToString.js
@@ -0,0 +1,11 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const fruits = ["Banana", {name: "Hassaan"}, true, 1, ["Mango", false],];
+const obj = {
+    name: "Ayan",
+    RollNo: 15
+}
+
+console.log(obj.toString());
+
+const text = fruits.toString();
+// console.log(text);
