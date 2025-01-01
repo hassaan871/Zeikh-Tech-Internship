8aaed369ea7b013ff0f484257f71cc46709a5191
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 11:21:21 +0500

    Includes method

---
+++ Javascript/Arrays/Day#02/Includes.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Apple", "Mango", "Gauva", "Cherry"];
+const inMyBucket = fruits.includes("Gauva");
+console.log(inMyBucket);
+
+const atPosition = fruits.includes("Apple",2);
+console.log(atPosition);
+
