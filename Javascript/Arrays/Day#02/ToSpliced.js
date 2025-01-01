60d9aca964b5668ecfb91777cf86c359b0aed429
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 17:21:06 +0500

    ToSpliced method

---
+++ Javascript/Arrays/Day#02/ToSpliced.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// At position 2, add "Lemon" and "Kiwi":
+// const fruits2 = fruits.toSpliced(2, 0, "Lemon", "Kiwi");
+const fruits2 = fruits.toSpliced(2, 1, "Lemon", "Kiwi");
+
+console.log(fruits2);
