e122d4fc1f73b2c7656ecfafd88665389215495f
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 12:08:25 +0500

    IsArray method

---
+++ Javascript/Arrays/Day#02/IsArray.js
@@ -0,0 +1,20 @@
+const fruits = ["Banana", "Orange", "Apple", "Watermelon", "PineApple", "Mango"];
+const result = Array.isArray(fruits);
+// console.log(result);
+
+// const name = "Hassaan";
+// const result1 = Array.isArray(name);
+// console.log(result1);
+
+// const name = ["Hassaan"];
+// const result1 = Array.isArray(name);
+// console.log(result1);
+
+const obj = {
+    students: ["Hassaan", "Ayan"],
+    rollNumbers: [63, 15]
+}
+
+// console.log(typeof obj);
+console.log(Array.isArray(obj));
+
