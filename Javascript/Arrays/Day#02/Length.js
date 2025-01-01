2bb2c0e1fa6a8640db5ae3dd42e39a472e86ac05
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 13:04:17 +0500

    Length property

---
+++ Javascript/Arrays/Day#02/Length.js
@@ -0,0 +1,8 @@
+const arr = ['a', 'b', 'c', 'd', 'e'];
+const arrLength = arr.length;
+
+arr.length = 2;
+console.log(`arrLength02: ${arr}`);
+console.log(`arrLength02: ${arr.length}`);
+
+console.log(`arrLength: ${arrLength}`);
