3cbd9c88267ad201fd1035c87ff69c36adb1b1fc
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 15:54:10 +0500

    Sort method

---
+++ Javascript/Arrays/Day#02/Sort.js
@@ -0,0 +1,6 @@
+const fruits = ["mango", "apple", "grapes", "watermelon","papaya"];
+fruits.sort();
+// console.log(fruits);
+
+const arr = ["mango", "zebra" ,true, 20, false, "apple", "OOP","Apple", "banana", "Papaya", ['a', 'r', 'r', 'a', 'y'], {zeikh:"Zeikh technologies"},{apple:"apple"}, {name: "Zeikh"}, 10];
+console.log(arr.sort());
