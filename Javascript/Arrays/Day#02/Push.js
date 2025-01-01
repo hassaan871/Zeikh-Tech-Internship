c46c3fea88b847908a4ee2dad5f5a42f886a47a9
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 14:25:30 +0500

    Push method

---
+++ Javascript/Arrays/Day#02/Push.js
@@ -0,0 +1,5 @@
+const alphabets = ["a","b","c"];
+console.log(`Length of alphabets before push: ${alphabets.length}`);
+alphabets.push("d");
+console.log(`Alphabets: ${alphabets}`);
+console.log(`Length of alphabets before push: ${alphabets.length}`);
