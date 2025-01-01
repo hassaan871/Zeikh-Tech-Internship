d9b9026dc73452e3ea4ee3e2a6773d870303fec7
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 15:13:57 +0500

    Slice method tested

--- Javascript/Arrays/Day#02/Slice.js
+++ Javascript/Arrays/Day#02/Slice.js
@@ -2,7 +2,8 @@ const alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m'];
 console.log(alph.length);
 
 // const newAplh = alph.slice(1,4);
-const newAplh = alph.slice(-4,-1);
+const newAplh = alph.slice(7,4);
+// const newAplh = alph.slice(-4,-1);
 // computed index = 13+(-4)=9
 // computed index = 13+(-1)=12
 
