ec61698b6ef628c27d69d0160b47e98f8e2dfcf5
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 11:35:04 +0500

    Includes method complete

--- Javascript/Arrays/Day#02/Includes.js
+++ Javascript/Arrays/Day#02/Includes.js
@@ -1,7 +1,19 @@
 const fruits = ["Banana", "Apple", "Mango", "Gauva", "Cherry"];
 const inMyBucket = fruits.includes("Gauva");
-console.log(inMyBucket);
+// console.log(inMyBucket);
 
 const atPosition = fruits.includes("Apple",2);
-console.log(atPosition);
+// console.log(atPosition);
+
+
+
+
+const arr = ["a","b","c","d","e"];
+// const isThere = arr.includes("a",-2);
+const isThere = arr.includes("d",-2);
+// computed index = length of array + fromIndex = 5+(-2)=3 false
+// if the computed index is less than or equal to 0 then the entire array will be searched.
+// const isThere = arr.includes("a",-100);
+console.log(isThere);
+
 
