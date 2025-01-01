ea097455a61d55da9884e075904b845ca8d3a03d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 11:42:32 +0500

    IndexOf method

---
+++ Javascript/Arrays/Day#02/IndexOf.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Apple","Orange", "Apple", "Mango"];
+const index = fruits.indexOf("Apple");
+const index1 = fruits.indexOf("Apple",1);
+const index2 = fruits.indexOf("Apple",2);
+
+console.log(index);
+// console.log(index1);
+// console.log(index2);
