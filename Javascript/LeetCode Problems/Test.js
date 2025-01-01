dadbf58d95b33643cd85a24c09d510663709e2aa
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 20:09:36 +0500

    deep copy shallow copy

---
+++ Javascript/LeetCode Problems/Test.js
@@ -0,0 +1,49 @@
+//Leet code problem
+
+/**
+ * 2635. Apply Transform Over Each Element in Array
+ */
+
+const arr = [1,2,3,4];
+function fn(num){
+    return num+1;
+}
+
+function Test(arr, fn){
+    let myArr = [];
+    const values  = arr.keys();
+    for(let i of values){
+        console.log(i);
+        
+        myArr[i] = fn(arr[i]);
+    }
+    // console.log(myArr.length);
+    
+    return myArr;
+}
+
+const result = Test(arr,fn);
+
+console.log(result);
+
+
+const arr1 = [
+    {
+        name: "umair",
+        arr: [1,2]
+    },
+    1,2
+]
+
+// const arr2 = [
+//     {
+//         name: "name",
+//         arr: [1,2]
+//     },
+//     1,2
+
+// ]
+const arr2 = structuredClone(arr1) // = [...arr1],  = arr1
+arr2[0].name = "Hassaan"
+
+console.log({arr1,arr2});
