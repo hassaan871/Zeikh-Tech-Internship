3e14cbf4763d73dfebbfe246d55fabfafbfb6403
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-01 17:14:00 +0500

    ToReversed method

---
+++ Javascript/Arrays/Day#02/ToReversed.js
@@ -0,0 +1,16 @@
+const arr = [0,1,2,3,4,5,6,7,8,9];
+
+const reversedArr = arr.toReversed();
+
+console.log(reversedArr);
+
+// Create an Array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Reverse the Array
+const fruits2 = fruits.toReversed();
+
+console.log(fruits2);
+
+
+
