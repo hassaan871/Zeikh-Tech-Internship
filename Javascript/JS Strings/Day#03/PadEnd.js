277c247449eecf2ca904c7af545bee6f299f0416
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 15:46:05 +0500

    PadStart string method

--- Javascript/JS Strings/Day#03/PadEnd.js
+++ Javascript/JS Strings/Day#03/PadEnd.js
@@ -6,3 +6,4 @@ const str1 = '200';
 console.log(str);
 console.log(newStr);
 console.log(str1.padEnd(5,"."));
+console.log(str1.padEnd(6,str1));
---
+++ Javascript/JS Strings/Day#03/PadStart.js
@@ -0,0 +1,8 @@
+// const str = '5';
+// const str = '50';
+const str = '50000';
+
+// const result = str.padStart(2, ".");
+const result = str.padStart(3, ".");
+
+console.log(result);
