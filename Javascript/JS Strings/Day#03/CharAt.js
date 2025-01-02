5c8ebcf67d0d8247a49f9e791563ff4d7ae9cc09
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 12:37:42 +0500

    CharAt string method

---
+++ Javascript/JS Strings/Day#03/CharAt.js
@@ -0,0 +1,10 @@
+let text = "Hello World";
+let character = text.charAt(0);
+let character1 = text.charAt(-1);
+let character2 = text.charAt(100);
+
+// console.log(character);
+// console.log(character1);
+// console.log(character2);
+let lastChar = text.charAt(text.length-1);
+console.log(lastChar);
