0826c120ae3d3f19965667dc3fced7c73fcfa539
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 11:58:25 +0500

    At method in detail

--- Javascript/JS Strings/Day#03/At.js
+++ Javascript/JS Strings/Day#03/At.js
@@ -5,4 +5,16 @@ console.log(`character00: ${character}`);
 console.log(`TypeOf: ${typeof intern}`);
 
 const character01 = intern[1];
-console.log(`character01: ${character01}`);
+console.log(`character[1]: ${character01}`);
+
+const character05 = intern[-1];
+console.log(`character[-1]: ${character05}`);
+
+const character02 = intern.at();
+console.log(`character(): ${character02}`);
+
+const character03 = intern.at(-1);
+console.log(`characterAt: -1: ${character03}`);
+
+const character04 = intern.at(-2);
+console.log(`characterAt: -2: ${character04}`);
