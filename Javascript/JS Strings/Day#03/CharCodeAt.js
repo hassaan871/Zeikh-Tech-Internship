09ae032b87846b225e52303ca061b3431172616a
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 12:43:51 +0500

    CharCodeAt string method

---
+++ Javascript/JS Strings/Day#03/CharCodeAt.js
@@ -0,0 +1,9 @@
+let text = "Hassaan";
+// let charCodeAt = text.charCodeAt();
+// let charCodeAt = text.charCodeAt(0);
+// let charCodeAt = text.charCodeAt(1);
+let charCodeAt = text.charCodeAt(text.length-1);
+
+
+// doesn't support negative indexing 
+console.log(charCodeAt);
