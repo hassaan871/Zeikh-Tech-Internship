a2686e16f63f189e711c3d6e791d04efbe71baec
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 16:31:47 +0500

    ReplaceAll String method

--- Javascript/JS Strings/Day#03/Replace.js
+++ Javascript/JS Strings/Day#03/Replace.js
@@ -1,23 +1,26 @@
-const text = "I think ferarri is faster than the lamborgini";
+// const text = "I think ferarri is faster than the lamborgini";
+const text = "I think ferarri is faster than the lamborgini, test test test";
 
-const newText = text.replace('ferarri', 'BUGHATTI');
+// const newText = text.replace('ferarri', 'BUGHATTI');
+// const newText = text.replace('ferarri', 'BUGHATTI');
+const newText = text.replace('test', 'REPLACED');
 
 console.log(text);
 console.log(newText);
 
-const newestText = text.replace('ferarri', `
-    ⢀⣀⣠⣤⣤⣶⣶⣤⣤⣄⣀⡀⠀⠀⠀⠀
-⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⠿⠿⠛⠛⠛⠉⠉⠉⠉⠛⠛⠛⠿⢿⣿⣿⣿
-⣿⣿⡇⠀⠀⠀⠀⠀⣤⣴⣷⡄⠀⠀⠀⠀⠀⢸⣿⣿
-⣿⣿⡇⠀⠀⠀⢀⠴⡄⣸⣿⡏⠀⠀⠀⠀⠀⢸⣿⣿
-⢹⣿⣷⠀⠀⠀⠁⡤⢶⣿⣿⣁⠀⢠⠀⠀⠀⣿⣿⡏
-⠈⢿⣿⣇⠀⠀⠀⢳⠀⢙⣿⣿⣷⠾⠀⠀⣸⣿⡿⠀
-⠀⠘⣿⣿⣆⠀⠀⠠⠤⠾⢹⡏⠀⠀⠀⣰⣿⣿⠃⠀
-⠀⠀⠘⢿⣿⣷⡀⠀⠀⠰⠟⠀⠀⢀⣾⣿⡿⠃⠀⠀
-⠀⠀⠀⠈⠻⣿⣿⣦⡀⠀⠀⢀⣴⣿⣿⠟⠁⠀⠀⠀
-⠀⠀⠀⠀⠀⠈⠻⣿⣿⣷⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀`);
+// const newestText = text.replace('ferarri', `
+//     ⢀⣀⣠⣤⣤⣶⣶⣤⣤⣄⣀⡀⠀⠀⠀⠀
+// ⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⠿⠿⠛⠛⠛⠉⠉⠉⠉⠛⠛⠛⠿⢿⣿⣿⣿
+// ⣿⣿⡇⠀⠀⠀⠀⠀⣤⣴⣷⡄⠀⠀⠀⠀⠀⢸⣿⣿
+// ⣿⣿⡇⠀⠀⠀⢀⠴⡄⣸⣿⡏⠀⠀⠀⠀⠀⢸⣿⣿
+// ⢹⣿⣷⠀⠀⠀⠁⡤⢶⣿⣿⣁⠀⢠⠀⠀⠀⣿⣿⡏
+// ⠈⢿⣿⣇⠀⠀⠀⢳⠀⢙⣿⣿⣷⠾⠀⠀⣸⣿⡿⠀
+// ⠀⠘⣿⣿⣆⠀⠀⠠⠤⠾⢹⡏⠀⠀⠀⣰⣿⣿⠃⠀
+// ⠀⠀⠘⢿⣿⣷⡀⠀⠀⠰⠟⠀⠀⢀⣾⣿⡿⠃⠀⠀
+// ⠀⠀⠀⠈⠻⣿⣿⣦⡀⠀⠀⢀⣴⣿⣿⠟⠁⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠈⠻⣿⣿⣷⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀`);
 
-console.log(newestText);
+// console.log(newestText);
---
+++ Javascript/JS Strings/Day#03/ReplaceAll.js
@@ -0,0 +1,10 @@
+const paragraph = "Xmen are the threat. Xmen, are they?";
+
+// const newParagraph = paragraph.replaceAll("Xmen","Avengers");
+// const newParagraph = paragraph.replaceAll("Xmen",false);
+// const newParagraph = paragraph.replaceAll("Xmen", 3.1415);
+const newParagraph = paragraph.replaceAll("Xmen", 9);
+// const newParagraph = paragraph.replaceAll("Xmen", {heros: 'Avengers'});
+
+console.log(paragraph);
+console.log(newParagraph);
