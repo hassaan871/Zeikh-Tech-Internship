e01b071675d42bf217b01b2dc23199fae60c28a9
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 17:54:34 +0500

    string method

---
+++ Javascript/JS Strings/Day#03/ToString.js
@@ -0,0 +1,25 @@
+const str = 'The quick brown fox jumps over the lazy dog.';
+const str1 = '  str1    ';
+
+// console.log(typeof str);
+
+// const tostr = str.toString();
+
+// console.log(typeof tostr);
+
+// const upperCase = str.toUpperCase();
+
+// console.log(upperCase);
+
+// const lowerCase = str.toLowerCase();
+
+// console.log(lowerCase);
+
+// const trimedStr = str1.trim();
+
+// console.log(trimedStr);
+
+console.log(str1.trimStart());
+console.log(str1.trimEnd());
+
+console.log(str1.valueOf());
