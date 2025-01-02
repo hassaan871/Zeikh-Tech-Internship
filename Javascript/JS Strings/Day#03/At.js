e465b1915857075c257b3c0a504b0ee84213889e
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 11:47:13 +0500

    At method

---
+++ Javascript/JS Strings/Day#03/At.js
@@ -0,0 +1,8 @@
+const intern = "Hasssaan";
+const character = intern.at(0);
+console.log(`character00: ${character}`);
+
+console.log(`TypeOf: ${typeof intern}`);
+
+const character01 = intern[1];
+console.log(`character01: ${character01}`);
