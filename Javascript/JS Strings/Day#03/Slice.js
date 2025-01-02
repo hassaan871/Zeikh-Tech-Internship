76f7392c854c4cafa6e1f73d211cbe39668e319d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 16:40:58 +0500

    Slice string method

---
+++ Javascript/JS Strings/Day#03/Slice.js
@@ -0,0 +1,15 @@
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const strLength = str.length;
+console.log(`strLength: ${strLength}`);
+
+
+// const sliced = str.slice(31);
+// const sliced = str.slice(1);
+// const sliced = str.slice(4,8);
+// const sliced = str.slice(4,9);
+// const sliced = str.slice(-4,-5);
+const sliced = str.slice(-4,-3);
+
+// console.log(str);
+console.log(sliced);
