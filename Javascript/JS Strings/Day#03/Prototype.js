2eec71baa9c16736d67e20277777c299e37ffa58
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 15:57:52 +0500

    Prototype string method

---
+++ Javascript/JS Strings/Day#03/Prototype.js
@@ -0,0 +1,25 @@
+// Javascript String prototype
+
+function Intern(name, exp, stack){
+    this.name = name;
+    this.exp = exp;
+    this.stack = stack;
+}
+
+Intern.prototype.company = "Zeikh Technologies";
+
+const newIntern = new Intern("Hassaan", '1 month', 'NodeJs');
+
+console.log(newIntern);
+
+console.log(newIntern.company);
+
+newIntern.company = "Zeikh Tech";
+
+console.log(newIntern.company);
+
+const newIntern1 = new Intern("Abdul Rehman", '6 months', 'ReactJs');
+
+console.log(newIntern1.company);
+
+
