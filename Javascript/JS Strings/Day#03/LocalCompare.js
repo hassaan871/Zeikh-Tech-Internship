897fd05d71727c60251eaf7ba7495b27667f2e77
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-02 15:10:44 +0500

    localCompare string method

---
+++ Javascript/JS Strings/Day#03/LocalCompare.js
@@ -0,0 +1,11 @@
+const a = "ReserVe";
+// const b = "reserve";
+const b = "reserve ";
+
+// console.log(a.localeCompare(b));
+// console.log(a.localeCompare(b));
+
+console.log("a".localeCompare("b"));
+console.log("b".localeCompare("a"));
+console.log("a".localeCompare("a"));
+
