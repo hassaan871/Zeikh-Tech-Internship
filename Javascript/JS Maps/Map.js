6643882e5f8a49f30f1083513e31cd111e561c16
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-03 16:56:13 +0500

    Sheraz code added

---
+++ Javascript/JS Maps/Map.js
@@ -0,0 +1,24 @@
+// let map = new Map();
+// let map = new Map({"key": "value"});
+let map = new Map([["key", "value"],["key1", "value1"]]);
+
+map.set('name', 'CUI');
+map.set(true, 1);
+map.set([1,2], {a:'1',b:'2'});
+
+
+// console.log(map);
+// console.log(map.get('name'));
+
+// console.log(typeof Map);
+console.log(typeof map);
+
+// console.log(map.has('CUI'));
+// console.log(map.has('name'));
+// console.log(map.has("key"));
+// console.log(map.has("key"));
+
+// console.log(map instanceof Map);
+
+
+
