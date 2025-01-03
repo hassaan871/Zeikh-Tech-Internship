eb5e396bf73fbdd074797314436a52da55dea518
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-03 11:29:36 +0500

    learning hoisting in js

---
+++ Javascript/JS Concepts/Hoisting.js
@@ -0,0 +1,12 @@
+// getName();
+console.log(x);
+console.log(getName);
+
+// const x = 7;
+var x = 7;
+
+function getName() {
+    console.log("Hassaan from Zeikh Tech");
+}
+
+// console.log(getName);
