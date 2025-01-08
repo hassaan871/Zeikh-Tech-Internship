ffd9efed1d0932f9d5769a0ee39a6eb57a494df7
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-08 12:56:03 +0500

    just tested block scope of let

---
+++ Javascript/Test copy.js
@@ -0,0 +1,11 @@
+// console.log(a);
+// let a;
+// a=10;
+
+// var b =10;
+// let b = 20;
+// console.log(window.b);
+console.log("Test copy");
+console.log(window.b);
+
+console.log({a});
--- Javascript/Test.js
+++ Javascript/Test.js
@@ -1,3 +1,31 @@
+// function func() {
+//     var b 
+//     console.log({b});
+    
+//     b =22
+
+// }
+
+// // console.log(a);
+// // let a;
+// // a=10;
+
+// let a = 20;
+// var b =10;
+// // console.log(window.b);
+// console.log("Test");
+// console.log(this.b);
+// console.log({a});
+
+// func()
+
+// const myfunc = () => {
+    
+// }
+
+
+{
+    let a = 10;
+
+}
 console.log(a);
-let a;
-a=10;
