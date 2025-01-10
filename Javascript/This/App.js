264239b9ba3b24b766604efda5053f1dfaa37dbf
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-10 18:54:23 +0500

    call

--- Javascript/This/App.js
+++ Javascript/This/App.js
@@ -1,5 +1,28 @@
-console.log(this);
+// "use strict";
 
-(function (){
-    console.log(this);
-})()
+// console.log(this);
+
+// (function (){
+//     // this value depends on strict and non strict mode
+//     console.log(this);
+// })()
+
+const student = {
+    name: "Hassaan",
+    printName: function(){
+        console.log(this.name);
+    }
+}
+
+const student2 = {
+    name: "Ammar"
+}
+
+student.printName();
+student.printName.call(student2);
+
+// function a(){
+//     console.log(this);
+    
+// }
+// a();
