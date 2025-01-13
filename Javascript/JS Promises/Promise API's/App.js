d4ce6f4b507eacd44da6174735aee8f6b09ba8c6
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-13 12:21:10 +0500

    Promise API Promise.all one promise rejected

--- Javascript/JS Promises/Promise API's/App.js
+++ Javascript/JS Promises/Promise API's/App.js
@@ -1,24 +1,29 @@
 const p1 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         resolve("P1 Success")
-    },3000);
+    },1000);
 })
 
 const p2 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        resolve("P2 Success")
-    },3000);
+        // resolve("P2 Success")
+        reject("P2 Rejected")
+    },2000);
 })
 
 const p3 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         resolve("P3 Success")
-    },3000);
+    },5000);
 })
 
 async function funcCall() {
-    const result = await Promise.all([p1, p2, p3]);
-    console.log(result);
+    try {
+        const result = await Promise.all([p1, p2, p3]);
+        console.log(result);
+    } catch (error) {
+        console.log("Error: ",error);
+    }
 }
 
 funcCall();
