3de11a2d3994e7d6b5e1d9c03df176242f831391
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-21 14:48:27 +0500

    Reverse String leetcode problem solved

---
+++ Javascript/LeetCode Problems/String/ReverseString.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse();
+};
+
+let s = ["h","e","l","l","o"];
+const result = reverseString(s);
+console.log(s);
+// console.log(result);
