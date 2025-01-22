f1c6ad1db2b8d38e8250f25b7dd918107a18765e
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-22 12:29:19 +0500

    just tried os module

--- Node Js/First App/app.js
+++ Node Js/First App/app.js
@@ -1,3 +1,15 @@
+const { log } = require('console');
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+log("totalMemory: ",totalMemory);
+log("freeMemory: ",freeMemory);
+
+
+/*
+
 // const { log } = require('console');
 const logger = require('./logger');
 const path = require('path');
@@ -10,3 +22,6 @@ const pathObj = path.parse(__filename);
 // log(pathObj);
 console.log("filename: ",__filename);
 console.log(pathObj);
+
+*/
+
