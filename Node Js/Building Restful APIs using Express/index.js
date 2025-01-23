cdff94a0384a9af7af5b72851c44ef2d3379633c
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-23 11:55:13 +0500

    query parameters and route parameters

--- Node Js/Building Restful APIs using Express/index.js
+++ Node Js/Building Restful APIs using Express/index.js
@@ -9,6 +9,17 @@ app.get('/api/courses', (req, res)=>{
     res.send([1,2,3])
 });
 
-app.listen(8000, ()=>{
-    console.log("Server is running on PORT 8000...");
-})
+app.get('/api/courses/:id', (req, res)=>{
+    res.send(`You want to get the Course ID: ${req.params.id}`);
+});
+
+app.get('/api/posts/:year/:month',(req, res)=>{
+    // res.send(req.params);
+    // res.send(`year: ${req.params.year}, month:${req.params.month}`)
+    res.send(req.query);
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, ()=>{
+    console.log(`Server is running on PORT ${port}...`);
+});
