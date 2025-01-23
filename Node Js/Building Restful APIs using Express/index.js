b2a5ab9b55c5c984c8a454f054380dc5617ad940
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-23 11:27:52 +0500

    created basic get api's using express, and installed nodemon

---
+++ Node Js/Building Restful APIs using Express/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Root...")
+});
+
+app.get('/api/courses', (req, res)=>{
+    res.send([1,2,3])
+});
+
+app.listen(8000, ()=>{
+    console.log("Server is running on PORT 8000...");
+})
