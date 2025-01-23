20596bddf906079c71d071d20649264a81bc3992
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-23 12:28:47 +0500

    handling http get request and also tested find array method

--- Javascript/Test.js
+++ Javascript/Test.js
@@ -1,71 +1,80 @@
-// function func() {
-//     var b 
-//     console.log({b});
+// // function func() {
+// //     var b 
+// //     console.log({b});
 
-//     b =22
+// //     b =22
 
-// }
+// // }
 
+// // // console.log(a);
+// // // let a;
+// // // a=10;
+
+// // let a = 20;
+// // var b =10;
+// // // console.log(window.b);
+// // console.log("Test");
+// // console.log(this.b);
+// // console.log({a});
+
+// // func()
+
+// // const myfunc = () => {
+
+// // }
+
+
+// // {
+// //     let a = 10;
+
+// // }
 // // console.log(a);
-// // let a;
-// // a=10;
 
-// let a = 20;
-// var b =10;
-// // console.log(window.b);
-// console.log("Test");
-// console.log(this.b);
-// console.log({a});
+// // function z() {
+// //     var b = 20;
+// //     return function x() {
+// //         var a = 7;
+// //         console.log("Testing");
+// //         return function y() {
+// //             console.log(`${a}, ${b}`);
+// //             console.log("Testing Again");
+// //         }
+// //         // a = 100;
+// //     }
+// // }
 
-// func()
-
-// const myfunc = () => {
-
+// function x(){
+//     const result = setTimeout(()=>{console.log("setTimeout")}, 1000);
+//     const result1 = setTimeout(()=>{console.log("setTimeout")}, 1000);
+//     console.dir({result,result1});
+//     console.log(typeof result);
+//     clearTimeout(result)
+//     // var a = 10;
+//     // function y(){
+//     //     var b = 20;
+//     //     function z() {
+//     //         console.log(`${a}, ${b}`);
+//     //     }
+//     //     z();
+//     // }
+//     // y();
+//     // const x = 100;
+//     // return x++;
 // }
+// x();
 
+// // var c = z();
+// // console.log(c);
+// // c();
+// // var z = x();
+// // console.log(z);
+// // z();
 
-// {
-//     let a = 10;
-
-// }
-// console.log(a);
-
-// function z() {
-//     var b = 20;
-//     return function x() {
-//         var a = 7;
-//         console.log("Testing");
-//         return function y() {
-//             console.log(`${a}, ${b}`);
-//             console.log("Testing Again");
-//         }
-//         // a = 100;
-//     }
-// }
-
-function x(){
-    const result = setTimeout(()=>{console.log("setTimeout")}, 1000);
-    const result1 = setTimeout(()=>{console.log("setTimeout")}, 1000);
-    console.dir({result,result1});
-    console.log(typeof result);
-    clearTimeout(result)
-    // var a = 10;
-    // function y(){
-    //     var b = 20;
-    //     function z() {
-    //         console.log(`${a}, ${b}`);
-    //     }
-    //     z();
-    // }
-    // y();
-    // const x = 100;
-    // return x++;
-}
-x();
-
-// var c = z();
-// console.log(c);
-// c();
-// var z = x();
-// console.log(z);
-// z();
+const courses = [
+    {id:1, course:"CS"},
+    {id:2, course:"SE"},
+    {id:3, course:"IT"}
+];
+const params = 1;
+const course = courses.find((element) => element.id === params);
+console.log("Course: ", course);
--- Node Js/Building Restful APIs using Express/index.js
+++ Node Js/Building Restful APIs using Express/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id:1, course:"CS"},
+    {id:2, course:"SE"},
+    {id:3, course:"IT"}
+];
+
 app.get('/', (req, res)=>{
     res.send("Root...")
 });
@@ -9,14 +15,19 @@ app.get('/api/courses', (req, res)=>{
     res.send([1,2,3])
 });
 
-app.get('/api/courses/:id', (req, res)=>{
-    res.send(`You want to get the Course ID: ${req.params.id}`);
-});
-
 app.get('/api/posts/:year/:month',(req, res)=>{
     // res.send(req.params);
     // res.send(`year: ${req.params.year}, month:${req.params.month}`)
     res.send(req.query);
+});
+
+app.get('/api/courses/:id',(req, res)=>{
+    let course = courses.find((element)=>element.id === parseInt(req.params.id));
+    if(!course){
+        res.status(404);
+        res.send(`Course with ID ${req.params.id} Not Found.`)
+    }
+    res.send(course);
 });
 
 const port = process.env.PORT || 8000;
