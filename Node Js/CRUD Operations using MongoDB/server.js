0c19793a9f4a6e256e0c6297ca903b5b4118f0d3
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-24 18:11:39 +0500

    creating a document

---
+++ Node Js/CRUD Operations using MongoDB/server.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(()=>{ console.log("Connected to Mongodb...")})
+    .catch((err)=>{ console.log("Error connecting mongodb...",err)});
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date , default: Date.now},
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course',courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Reactjs Course',
+        author: 'code with mosh',
+        tags: ['node', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
