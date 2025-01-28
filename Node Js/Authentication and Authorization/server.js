53244c70b6b70a343d1067c8fc5fc1f2e03b5d63
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-28 13:17:16 +0500

    using lodash

--- Node Js/Authentication and Authorization/server.js
+++ Node Js/Authentication and Authorization/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -61,14 +62,17 @@ async function registeredUserMiddleware(req, res, next) {
 
 //routes
 app.post('/registerUser/', validateUserMiddleware, registeredUserMiddleware, async (req, res) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
+        const user = new User(
+            _.pick(req.body, ['name','email','password'])
+        //     {
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     password: req.body.password
+        // }
+    );
 
         await user.save();
-        return res.send(user);
+        return res.send(_.pick(user, ['_id','name','email']));
     });
 
 app.listen(PORT, () => {
