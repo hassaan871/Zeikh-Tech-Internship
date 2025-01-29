e983a809ac072e8e91a96c0662df74b05b3dd631
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-29 15:57:44 +0500

    user model is admin property added

--- Projects/P01-Zeikh Task Manager/models/user.model.js
+++ Projects/P01-Zeikh Task Manager/models/user.model.js
@@ -16,6 +16,10 @@ const user = new mongoose.Schema({
         type: String,
         required: true,
         minlength:5
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 }, { timestamps: true });
 
