057b76d955d19646763840e5653a96a6a563af83
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-29 15:41:23 +0500

    server setup

--- Projects/P01-Zeikh Task Manager/index.js
+++ Projects/P01-Zeikh Task Manager/index.js
@@ -1,3 +1,16 @@
 const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
 
 connectDB();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server running on PORT ${PORT}`);
+    })
+} catch (error) {
+    console.log(`Error while running the server: ${error}`);
+}
