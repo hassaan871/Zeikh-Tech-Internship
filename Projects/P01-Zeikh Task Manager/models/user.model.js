ec8fefff0c385708ac0581889c5aef3d682713b3
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-29 15:54:02 +0500

    user model created

---
+++ Projects/P01-Zeikh Task Manager/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        minlength: 5,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength:5
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', user);
+module.exports = User;
