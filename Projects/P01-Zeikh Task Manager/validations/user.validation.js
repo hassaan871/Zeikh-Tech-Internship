788f0c52f46b1942703064e1e22aa854bebef9ac
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-30 11:40:04 +0500

    Joi login user validation

--- Projects/P01-Zeikh Task Manager/validations/user.validation.js
+++ Projects/P01-Zeikh Task Manager/validations/user.validation.js
@@ -9,6 +9,14 @@ const validateUser = (user) =>{
     return userSchema.validate(user);
 }
 
+const validateLoginUser = (user) => {
+    const userSchema = Joi.object({
+        email: Joi.string().min(5).required().email(),
+        password: Joi.string().min(5).required()
+    });
+    return userSchema.validate(user);
+}
+
 module.exports = {
     validateUser
 }
