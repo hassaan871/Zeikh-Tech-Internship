d7001212713af15205cd27af21f5ce74c54f09d1
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-01-31 16:12:36 +0500

    admin controller: get all users

---
+++ Projects/P01-Zeikh Task Manager/controllers/admin.controller.js
@@ -0,0 +1,15 @@
+const User = require('../models/user.model');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = awaitUser.find({isAdmin: false});
+        if(!users) return res.status(404).json({empty: "no user found"});
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports = {
+    getAllUsers
+}
