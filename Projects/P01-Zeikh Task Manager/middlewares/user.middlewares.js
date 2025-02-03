3af3ab32030409f45c8819d1e9059fe07120c807
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-02-03 15:24:33 +0500

    Login user bug fixed

--- Projects/P01-Zeikh Task Manager/middlewares/user.middlewares.js
+++ Projects/P01-Zeikh Task Manager/middlewares/user.middlewares.js
@@ -4,47 +4,54 @@ const { validateUser, validateLoginUser, validateUsername, validatePassword, val
 
 const validateUserMiddleware = (req, res, next) => {
     const { error } = validateUser(req.body);
-    if(error) return res.status(400).json({ error: error.details[0].message});
+    if (error) return res.status(400).json({ error: error.details[0].message });
     next();
 }
 
 const validateLoginUserMiddleware = (req, res, next) => {
     const { error } = validateLoginUser(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+    if (error) return res.status(400).json({ error: error.details[0].message });
     next();
 }
 
 const validDbUserMiddleware = async (req, res, next) => {
 
-    let user;
-    
-    if(req.body.email) user = await User.findOne({email: req.body.email});
-    if(req.body.username) user = await User.findOne({username: req.body.username});
+        let user;
 
-    // const validPassword = await bcrypt.compare(req.body.password, user.password);
-    const validPassword = req.body.password === user.password ? true : false; 
+        if (req.body.email) {
+            user = await User.findOne({ email: req.body.email });
+            if (!user) return res.status(404).json({ error: "Invalid email" });
+        }
 
-    if(!user || !validPassword) return res.status(401).json({error: "Invalid email or password!"});
+        if (req.body.username) {
+            user = await User.findOne({ username: req.body.username });
+            if (!user) return res.status(404).json({ error: "Invalid username" });
+        }
 
-    req.user = user;
-    next();
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        // const validPassword = req.body.password === user.password ? true : false; 
+
+        if (!validPassword) return res.status(401).json({ error: "Invalid password!" });
+
+        req.user = user;
+        next();
 }
 
 const validateUsernameMiddleware = (req, res, next) => {
-    const {error} = validateUsername(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+    const { error } = validateUsername(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
     next();
 }
 
 const validateEmailMiddleware = (req, res, next) => {
-    const {error} = validateEmail(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+    const { error } = validateEmail(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
     next();
 }
 
 const validatePasswordMiddleware = (req, res, next) => {
-    const {error} = validatePassword(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+    const { error } = validatePassword(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
     next();
 }
 
